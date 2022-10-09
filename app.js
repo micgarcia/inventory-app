66fab78bd22f1780cf686d08e2d7a95f071ec7d4
micgarcia <micgarcia9185@gmail.com>
2022-10-08 18:06:41 -0700

    added nodemon dependency, created Cars and Vehicle model

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const mongoose = require('mongoose');
+const mongoDB = "mongodb+srv://inv-app:invapp01@cluster0.qoiibzv.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
---
+++ models/vehicle.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const VehicleSchema = new Schema({
+  name: { type: String, required: true, maxLength: 100},
+  description: { type: String, required: true},
+  category: { type: Schema.Types.ObjectId, refPath: "car_type"},
+  car_type: { type: String, enum: ['Cars, Trucks'], required: true},
+  price: { type: Number, required: true},
+  stock: { type: Number, required: true},
+  trim: { type: String, required: true, maxLength: 50}
+});
+
+VehicleSchema.virtual("url").get(function() {
+  return `catalog/${this.car_type}`;
+});
+
+module.exports = mongoose.model("Vehicle", VehicleSchema);
